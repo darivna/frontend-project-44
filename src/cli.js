f3c0b8f23e30a43e1ed718410e19e35188a75bab
Daria <daria@Darias-MBP.lan>
2022-09-02 11:38:55 +0300

    feat: add logic into brain-even game

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default function askUserName() {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 }
