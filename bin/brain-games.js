23e53e69741db9d791b40f3a73c2e6cddc9d8d96
Daria <daria@Darias-MBP.lan>
2022-09-27 20:19:42 +0300

    fix: connecting readlyne-sync

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const readlineSync = require('readline-sync');
+
 console.log('Welcome to the Brain Games!');
 
 const userName = readlineSync.question('May I have your name? ');
